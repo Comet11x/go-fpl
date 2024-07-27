21cfe2c6e0fcb0fc3c601927273f52bea7273d81
comet11x <comet11x@protonmail.com>
2024-07-27 21:55:47 +0300

    feat[test]: update test of Result

--- pkg/core/result_test.go
+++ pkg/core/result_test.go
@@ -21,10 +21,22 @@ func TestCreateErrResult(t *testing.T) {
 	}
 }
 
+
+func TestUnwrapPtrForOk(t *testing.T) {
+	i := 1
+	r := Ok[int](i)
+	o := r.UnwrapPtr()
+	if i != *o {
+		t.Fatalf("it must be equal %d", i)
+	}
+}
+
+
+
 func TestUnwrapForOk(t *testing.T) {
 	s := "foo"
 	r := Ok[string](s)
-	if r.Ok().Unwrap() != s {
+	if r.Unwrap() != s {
 		t.Fatalf("it must be equal %s ", s)
 	}
 
@@ -110,5 +122,52 @@ func TestUnwrapOrPtrForError(t *testing.T) {
 
 	if r.Error().Unwrap() == nil {
 		t.Fatal("it must be not equal nil")
+	}
+}
+
+
+func TestOkPtrForOk(t *testing.T) {
+	v := 1
+	r := Ok[int](v)
+	
+	o := r.OkPtr()
+
+	if o.IsNone() || !o.IsSome() {
+		t.Fatal("it mut be Some[int]")
+	}
+}
+
+func TestOkPtrForErr(t *testing.T) {
+	r := Err[int](errors.New("error"))
+	
+	o := r.OkPtr()
+
+	if !o.IsNone() || o.IsSome() {
+		t.Fatal("it mut be None[int]")
+	}
+}
+
+func TestToTupleForOk(t *testing.T) {
+	v1 := 1
+	r := Ok[int](v1)
+
+	v2, err := r.ToTuple()
+
+	if v1 != v2  {
+		t.Fatal("it must be equal")
+	}
+
+	if err != nil {
+		t.Fatal("it must be not equal")
+	}
+}
+
+func TestToTupleForErr(t *testing.T) {
+	r := Err[int](errors.New("error"))
+
+	_, err := r.ToTuple()
+
+	if err == nil {
+		t.Fatal("it must be not equal")
 	}
 }
