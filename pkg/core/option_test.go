2fe00b64c670376475ee8ff55bf623bd67b3a54e
comet11x <comet11x@protonmail.com>
2024-07-27 20:15:27 +0300

    feat[test]: update tests for Option

--- pkg/core/option_test.go
+++ pkg/core/option_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestCreateNone(t *testing.T) {
+	o := None[string]()
+	if o.IsSome() {
+		t.Fatal("it must be Noe[string]")
+	}
+}
+
+
 func TestCreateSomeForString(t *testing.T) {
 	o := Some("foo")
 	if o.IsNone() {
@@ -15,5 +23,59 @@ func TestCreateSomeOfString(t *testing.T) {
 	o := Some("foo")
 	if o.Unwrap() != "foo" {
 		t.Fatal("a value must be equal")
+	}
+}
+
+func TestMapNoneForNone(t *testing.T) {
+	o1 := None[string]()
+	o2 := MapNone[string, int](o1, func ()  int {
+		return 1
+	})
+
+	if o2.IsNone() {
+		t.Fatal("it must be Some[int]")
+	}
+
+	if o2.Unwrap() != 1 {
+		t.Fatal("it must be equal 0")
+	}
+}
+
+func TestMapNoneForSome(t *testing.T) {
+	o1 := Some[string]("foo")
+	o2 := MapNone[string, int](o1, func ()  int {
+		return 1
+	})
+
+	if o2.IsSome() {
+		t.Fatal("it must be None[int]")
+	}
+}
+
+
+func TestMapSomeForSome(t *testing.T) {
+	s := "string"
+	o1 := Some[string](s)
+	o2 := MapSome[string, int](o1, func (s string)  int {
+		return len(s)
+	})
+
+	if o2.IsNone() {
+		t.Fatal("it must be Some[int]")
+	}
+
+	if o2.Unwrap() != len(s) {
+		t.Fatalf("it mut be equal %d", len(s))
+	}
+}
+
+func TestMapSomeForNone(t *testing.T) {
+	o1 := None[string]()
+	o2 := MapSome[string, int](o1, func (s string)  int {
+		return len(s)
+	})
+
+	if o2.IsSome() {
+		t.Fatal("it must be None[int]")
 	}
 }
