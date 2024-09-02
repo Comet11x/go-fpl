6925ebab69109a402fc491d74edbc0b41eb0d45d
comet11x <comet11x@protonmail.com>
2024-09-02 22:17:22 +0300

    feat!: rename type args to Args

--- pkg/async/promise.type.go
+++ pkg/async/promise.type.go
@@ -4,4 +4,4 @@ import "github.com/comet11x/go-fpl/pkg/core"
 
 type Resolve[T any] func(T)
 type Reject func(any)
-type args[T any] core.Pair[Resolve[T], Reject]
+type Args[T any] core.Pair[Resolve[T], Reject]
