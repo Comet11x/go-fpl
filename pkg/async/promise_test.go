25c4c5f73cee29b5361c4f5a656aed0fb3ce23dc
comet11x <comet11x@protonmail.com>
2024-09-02 22:20:58 +0300

    feat: add promise_test

---
+++ pkg/async/promise_test.go
@@ -0,0 +1,28 @@
+package async
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestPromise(t *testing.T) {
+	done := atomic.Bool{}
+	promise := Async[string](func(resolve func(string), reject func(any)) {
+		time.Sleep(time.Second * 2)
+		resolve("Hello")
+	})
+
+	promise.Then(func(s string) {
+		done.Store(true)
+		fmt.Println("A value from the promise: ", s)
+	})
+
+	promise.Await()
+
+	time.Sleep(time.Second * 3)
+	if !done.Load() {
+		t.Fatal("it must be true")
+	}
+}
