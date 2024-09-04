cd3b5c1420869b03346107880abb7717dc29f92e
comet11x <comet11x@protonmail.com>
2024-09-04 18:11:56 +0300

    feat: update constructors of Promise

--- pkg/async/promise.ctor.go
+++ pkg/async/promise.ctor.go
@@ -9,9 +9,9 @@ import (
 func Async[T any](fn func(resolve func(T), reject func(any))) Promise[T] {
 	m := &sync.Mutex{}
 	p := promise[T]{
-		thenHandler:    make([]Resolve[T], 0),
-		catchHandler:   make([]Reject, 0),
-		finallyHandler: make([]func(), 0),
+		thenHandler:    make([]ResolveHandler[T], 0),
+		catchHandler:   make([]RejectedHandler, 0),
+		finallyHandler: make([]FinallyHandler, 0),
 		resultMutex:    m,
 		cond:           sync.NewCond(m),
 	}
@@ -22,42 +22,73 @@ func Async[T any](fn func(resolve func(T), reject func(any))) Promise[T] {
 
 func Then[T any, U any](p Promise[T], fn func(T) U) Promise[U] {
 	return Async[U](func(resolve func(U), reject func(any)) {
-		e := p.Await()
-		if e.IsLeft() {
-			try := core.Call(fn, e.UnwrapLeft())
-			if try.IsSuccess() {
-				resolve(try.Success().Unwrap())
-			} else {
-				reject(try.Failure().Unwrap())
-			}
-		} else {
-			reject(e.UnwrapRight())
-		}
+		p.Await().
+			IfLeft(func(value T) {
+				core.Call(fn, value).
+					IfSuccess(resolve).
+					IfFailure(reject)
+			}).
+			IfRight(reject)
 	})
 }
 
 func ThenFrom[T any, U any](p Promise[T], fn func(T) Promise[U]) Promise[U] {
 	return Async[U](func(resolve func(U), reject func(any)) {
-		e := p.Await()
-		if e.IsLeft() {
-			e := fn(e.UnwrapLeft()).Await()
-			if e.IsLeft() {
-				resolve(e.UnwrapLeft())
-			} else {
-				reject(e.UnwrapRight())
-			}
-		} else {
-			reject(e.UnwrapRight())
+		p.Await().
+			IfLeft(func(value T) {
+				fn(value).Await().
+					IfLeft(resolve).
+					IfRight(reject)
+			}).
+			IfRight(reject)
+	})
+}
+
+func PromiseAllSettled[T any](promises ...Promise[T]) Promise[[]core.Either[T, any]] {
+	return Async(func(resolve func([]core.Either[T, any]), reject func(any)) {
+		out := make([]core.Either[T, any], len(promises))
+		for i, p := range promises {
+			out[i] = p.Await()
 		}
+		resolve(out)
 	})
 }
 
 func PromiseAll[T any](promises ...Promise[T]) Promise[[]T] {
-
-	return nil
+	return Async(func(resolve func([]T), reject func(any)) {
+		if len(promises) == 0 {
+			resolve(make([]T, 0))
+			return
+		}
+		out := make([]T, 0, len(promises))
+		var e core.Either[T, any]
+		for _, p := range promises {
+			e = p.Await()
+			if e.IsRight() {
+				break
+			} else {
+				out = append(out, e.UnwrapLeft())
+			}
+		}
+		e.IfLeft(func(_ T) {
+			resolve(out)
+		}).IfRight(reject)
+	})
 }
 
-func PromiseAny[T any](promises ...Promise[T]) Promise[[]T] {
-
-	return nil
+func PromiseAny[T any](promises ...Promise[T]) Promise[core.Option[T]] {
+	return Async(func(resolve func(core.Option[T]), reject func(any)) {
+		if len(promises) == 0 {
+			resolve(core.None[T]())
+			return
+		}
+		var e core.Either[T, any]
+		for _, p := range promises {
+			e = p.Await()
+			if e.IsLeft() {
+				break
+			}
+		}
+		e.IfLeft(func(v T) { resolve(core.Some(v)) }).IfRight(reject)
+	})
 }
